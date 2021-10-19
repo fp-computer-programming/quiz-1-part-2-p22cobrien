19dbc2b77beeec4eabca252ff7afedb78994a3f2
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-19 10:49:27 -0400

    update

--- quiz-1-part-2.py
+++ quiz-1-part-2.py
@@ -8,15 +8,21 @@ y1 = year
 
 if month == 1 or 2:
     y1 -= 1
+else:
+    y1 = year
+
 if month == 1:
     m1 = 13
 if month == 2:
     m1 = 14
+else:
+    m1 = month
 
 j = y1 // 100
 k = y1 % 100
 
-h = (day + (26 * (m1 + 1)) // 10 + k + (k // 4) + (j // 4) + (5 * j)) % 7
+h = (day + ((26 * (m1 + 1)) // 10) + k + (k // 4) + (j // 4) + (5 * j)) % 7
+h += 1
 
 if h == 0:
     print(str(month) + "/" + str(day) + "/" + str(year) + " was a Saturday.")
@@ -30,5 +36,5 @@ elif h == 4:
     print(str(month) + "/" + str(day) + "/" + str(year) + " was a Wednesday.")
 elif h == 5:
     print(str(month) + "/" + str(day) + "/" + str(year) + " was a Thursday.")
-elif h == 6:
+elif h == 7:
     print(str(month) + "/" + str(day) + "/" + str(year) + " was a Friday.")
