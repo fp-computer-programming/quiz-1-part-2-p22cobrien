00c29c154fdf53bc6eec709ed7a53d71167efe6f
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-19 10:36:30 -0400

    submit Quiz 1 part 2

---
+++ quiz-1-part-2.py
@@ -0,0 +1,34 @@
+# Author: CMOB 19/10/2021
+
+day = int(input("What is the day of the month? "))
+month = int(input("What is the month? "))
+year = int(input("what is the year? "))
+
+y1 = year
+
+if month == 1 or 2:
+    y1 -= 1
+if month == 1:
+    m1 = 13
+if month == 2:
+    m1 = 14
+
+j = y1 // 100
+k = y1 % 100
+
+h = (day + (26 * (m1 + 1)) // 10 + k + (k // 4) + (j // 4) + (5 * j)) % 7
+
+if h == 0:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Saturday.")
+elif h == 1:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Sunday.")
+elif h == 2:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Monday.")
+elif h == 3:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Tuesday.")
+elif h == 4:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Wednesday.")
+elif h == 5:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Thursday.")
+elif h == 6:
+    print(str(month) + "/" + str(day) + "/" + str(year) + " was a Friday.")
